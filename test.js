af6320fa2901d2f42436e343194e8e50823f1802
Jesse Patching <jesse.patching@ucalgary.ca>
2019-09-10 14:55:07 -0600

    Make empty tests fail by default

--- test.js
+++ test.js
@@ -11,17 +11,21 @@ describe("Browser Tests", function () {
 
   it('Verify h1 tag is "Search"', function () {
     // @TODO
+    assert.equal(true, false);
   });
 
   it('Verify Search input placeholder attribute is "Search Query"', function () {
     // @TODO
+    assert.equal(true, false);
   });
 
   it('Verify Search input is empty', function () {
     // @TODO
+    assert.equal(true, false);
   });
 
   it('Change search query to "Test", click Search button, and verify message "You successfully searched for Test"', function () {
     // @TODO
+    assert.equal(true, false);
   });
 });
